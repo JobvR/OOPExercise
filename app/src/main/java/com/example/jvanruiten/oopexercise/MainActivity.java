982aa0c815a4427817405dedcfc2d07df67315ae
jvanruiten <jobadri@live.nl>
2018-08-23 14:20:54 +0300

    Created methods createDogs() and addDogs()
    Initialized the 9 dogs before onCreate

--- app/src/main/java/com/example/jvanruiten/oopexercise/MainActivity.java
+++ app/src/main/java/com/example/jvanruiten/oopexercise/MainActivity.java
@@ -22,33 +22,52 @@ public class MainActivity extends AppCompatActivity {
     public ArrayList<String> dogBreeds = new ArrayList<>();
     public ArrayList<String> dogColors = new ArrayList<>();
     public ArrayList<Integer> dogHeights = new ArrayList<>();
+    public Labrador amber;
+    public Labrador beau;
+    public Labrador cyra;
+    public Bulldog darryl;
+    public Bulldog elliot;
+    public Bulldog fido;
+    public Poodle george;
+    public Poodle harry;
+    public Poodle isabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Labrador amber = new Labrador("Labrador", "Blond", 105);
+        createDogs();
+        addDogs();
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(dogNames, dogBreeds, dogColors, dogHeights);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+    public void createDogs(){
+        amber = new Labrador("Labrador", "Blond", 105);
         amber.setName("Amber");
-        Labrador beau = new Labrador("Labrador", "Black", 95);
+        beau = new Labrador("Labrador", "Black", 95);
         beau.setName("Beau");
-        Labrador cyra = new Labrador("Labrador", "Brown", 100);
+        cyra = new Labrador("Labrador", "Brown", 100);
         cyra.setName("Cyra");
 
-        Bulldog darryl = new Bulldog("Bulldog", "Grey", 35);
+        darryl = new Bulldog("Bulldog", "Grey", 35);
         darryl.setName("Darryl");
-        Bulldog elliot = new Bulldog("Bulldog", "Black and white", 40);
+        elliot = new Bulldog("Bulldog", "Black and white", 40);
         elliot.setName("Elliot");
-        Bulldog fido = new Bulldog("Bulldog", "Brown", 37);
+        fido = new Bulldog("Bulldog", "Brown", 37);
         fido.setName("Fido");
 
-        Poodle george = new Poodle("Poodle", "White", 35);
+        george = new Poodle("Poodle", "White", 35);
         george.setName("George");
-        Poodle harry = new Poodle("Poodle", "Black", 40);
+        harry = new Poodle("Poodle", "Black", 40);
         harry.setName("Harry");
-        Poodle isabel = new Poodle("Poodle", "Pink", 37);
+        isabel = new Poodle("Poodle", "Pink", 37);
         isabel.setName("Isabel");
-
+    }
+    public void addDogs(){
         dogNames.add(amber.getName());
         dogNames.add(beau.getName());
         dogNames.add(cyra.getName());
@@ -58,16 +77,6 @@ public class MainActivity extends AppCompatActivity {
         dogNames.add(george.getName());
         dogNames.add(harry.getName());
         dogNames.add(isabel.getName());
-
-//        dogNames.add("1");
-//        dogNames.add("2");
-//        dogNames.add("3");
-//        dogNames.add("4");
-//        dogNames.add("5");
-//        dogNames.add("6");
-//        dogNames.add("7");
-//        dogNames.add("8");
-//        dogNames.add("9");
 
         dogBreeds.add(amber.getBreed());
         dogBreeds.add(beau.getBreed());
@@ -98,10 +107,5 @@ public class MainActivity extends AppCompatActivity {
         dogHeights.add(george.getHeight());
         dogHeights.add(harry.getHeight());
         dogHeights.add(isabel.getHeight());
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(dogNames, dogBreeds, dogColors, dogHeights);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
